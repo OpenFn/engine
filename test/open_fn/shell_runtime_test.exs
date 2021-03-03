8c621faab2a86695ec2933d8585c0e2e2fa76fc4
Stuart Corbishley <corbish@gmail.com>
2021-03-03 16:46:38 +0200

    Working test code for dealing with partial bitstrings from Rambo

--- test/open_fn/shell_runtime_test.exs
+++ test/open_fn/shell_runtime_test.exs
@@ -3,7 +3,49 @@ defmodule OpenFn.ShellRuntimeTest do
 
   alias OpenFn.RunSpec
 
-  # test "works" do
-  #   {:ok, %Rambo{}} = OpenFn.ShellRuntime.run(%RunSpec{})
-  # end
+  @tag skip: true
+  test "works" do
+    {:ok, %Rambo{}} = OpenFn.ShellRuntime.run(%RunSpec{})
+  end
+
+  test "can deal with bitstrings" do
+    {:ok, agent} = Agent.start_link(fn -> {[], "", ""} end)
+
+    f = fn {_type, data} ->
+      Agent.update(agent, fn {lines, current_chunk, pending} ->
+        next_chunk = pending <> data
+
+        {state, chunk, pending} =
+          Enum.reduce_while(
+            0..byte_size(next_chunk),
+            {current_chunk, String.next_grapheme(next_chunk)},
+            fn _, {chunk, grapheme_result} ->
+              case grapheme_result do
+                {<<_::utf8>> = next_char, rest} ->
+                  {:cont, {chunk <> IO.iodata_to_binary(next_char), String.next_grapheme(rest)}}
+
+                {next_char, rest} ->
+                  {:halt, {:incomplete, chunk, next_char <> rest}}
+
+                nil ->
+                  {:halt, {:done, chunk, ""}}
+              end
+            end
+          )
+
+        case state do
+          :incomplete ->
+            {lines, chunk, pending}
+
+          :done ->
+            {lines ++ [chunk], nil, pending}
+        end
+      end)
+    end
+
+    Rambo.run("echo", [String.duplicate(".", 63) <> "💣"], log: f)
+
+    assert Agent.get(agent, fn value -> value end) ==
+             {["...............................................................💣\n"], nil, ""}
+  end
 end
