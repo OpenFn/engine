7047f51970a126fd7aed929a4f8f1d2bb4056adc
Stuart Corbishley <corbish@gmail.com>
2021-03-04 16:55:00 +0200

    Incrementally build up log messages into a stream

--- test/open_fn/shell_runtime_test.exs
+++ test/open_fn/shell_runtime_test.exs
@@ -8,44 +8,24 @@ defmodule OpenFn.ShellRuntimeTest do
     {:ok, %Rambo{}} = OpenFn.ShellRuntime.run(%RunSpec{})
   end
 
-  test "can deal with bitstrings" do
-    {:ok, agent} = Agent.start_link(fn -> {[], "", ""} end)
+  test "when used with LogStream" do
+    {line_stream, stream_callback} = OpenFn.LogStream.create()
 
-    f = fn {_type, data} ->
-      Agent.update(agent, fn {lines, current_chunk, pending} ->
-        next_chunk = pending <> data
-
-        {state, chunk, pending} =
-          Enum.reduce_while(
-            0..byte_size(next_chunk),
-            {current_chunk, String.next_grapheme(next_chunk)},
-            fn _, {chunk, grapheme_result} ->
-              case grapheme_result do
-                {<<_::utf8>> = next_char, rest} ->
-                  {:cont, {chunk <> IO.iodata_to_binary(next_char), String.next_grapheme(rest)}}
-
-                {next_char, rest} ->
-                  {:halt, {:incomplete, chunk, next_char <> rest}}
-
-                nil ->
-                  {:halt, {:done, chunk, ""}}
-              end
-            end
-          )
-
-        case state do
-          :incomplete ->
-            {lines, chunk, pending}
-
-          :done ->
-            {lines ++ [chunk], nil, pending}
-        end
+    task =
+      Task.async(fn ->
+        Rambo.run("echo", [String.duplicate(".", 63) <> "💣"], log: stream_callback)
+        Process.sleep(100)
+        Rambo.run("echo", [String.duplicate(".", 63) <> "💣\n"], log: stream_callback)
+        Rambo.run("echo", [String.duplicate(".", 63) <> "💣"], log: stream_callback)
+        stream_callback.(:complete)
       end)
-    end
 
-    Rambo.run("echo", [String.duplicate(".", 63) <> "💣"], log: f)
+    assert line_stream |> Enum.into([]) == [
+             "...............................................................💣\n",
+             "...............................................................💣\n\n",
+             "...............................................................💣\n"
+           ]
 
-    assert Agent.get(agent, fn value -> value end) ==
-             {["...............................................................💣\n"], nil, ""}
+    Task.await(task)
   end
 end
