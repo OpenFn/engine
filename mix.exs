93f3f4371bb03d349122bf7433b1f7507b580441
Taylor Downs <taylor@openfn.org>
2022-05-24 08:45:53 +0100

    use rambo with arm64 support

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Engine.MixProject do
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:opq, github: "fredwu/opq", ref: "08406f5"},
       {:quantum, "~> 3.4"},
-      {:rambo, "~> 0.3.4"},
+      {:rambo, github: "myobie/rambo", ref: "e321db8"},
       {:temp, "~> 0.4"},
       {:yaml_elixir, "~> 2.5"}
     ]
